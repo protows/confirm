1010792c8b19e558e5989c62a1d8c75992e8f24f
protows <38029969+protows@users.noreply.github.com>
2018-04-04 22:31:03 -0700

    Update bbc.py

--- bbc.py
+++ bbc.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver", log_path="/usr/local/bin/geckodriver.log")	
+        self.driver = webdriver.Firefox()	
     	
         
     def test_search_in_python_org(self):
